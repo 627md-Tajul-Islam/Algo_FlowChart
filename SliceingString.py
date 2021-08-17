09e42404fe3a4e1a75094e3d4383a230868b1f8c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-16 20:24:21 -0700

    3rd chapter SliceString added

---
+++ SliceingString.py
@@ -0,0 +1,8 @@
+string = "Hello Mahadee!"
+
+print(string) # print the complete string
+print(string[0]) # print the First Charecter
+print(string[2:5]) # print the Charecter 3rd to fifth
+print(string[2:]) # print starting from 3rd charecter
+print(string * 2) # print string 2 times
+print(string + "Welcome") # print concatenated string
