57da86736d7063d11e55814ec295ccb5d4181455
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-16 20:43:37 -0700

    3rd chapter List added

---
+++ List.py
@@ -0,0 +1,9 @@
+list = ["Mahi", 786,2.23,'Mahdee', 70.2]
+tinyList = [123, 'Mahdee']
+
+print(list) # print the complete list
+print(list[0]) # print the first element
+print(list[1:3]) # print the 2nd to 3rd
+print(list[2:]) # print list starting from 3rd
+print(tinyList*2) # print list double
+print(list + tinyList)
