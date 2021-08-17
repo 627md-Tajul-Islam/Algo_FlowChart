2db435677fddd2b228b213de155bb0796606e267
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-16 23:13:45 -0700

    3rd chapter Dictionary added with extra example

---
+++ Dictionary.py
@@ -0,0 +1,21 @@
+dict = {}
+dict['one'] = "This is one"
+dict['2'] = "This is two"
+
+tinyDict = {'name': 'john',
+            'code':1234,
+            'dept': 'web'}
+
+print(dict) # print the full dictionary
+print(dict['one']) # print the value for one
+print(dict['2']) # print the value for two
+print(tinyDict) #print full dictionary
+print(tinyDict.keys()) # print only the keys
+print(tinyDict.values()) #print all values
+
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict["brand"])
