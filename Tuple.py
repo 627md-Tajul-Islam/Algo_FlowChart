9fea32b9760a038573b708df06b56381968a8e72
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-16 21:54:19 -0700

    3rd chapter Tuple added

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+tuple = ("Mahi",786,2.73,'Mahdee', 70.2)
+tinyTuple = (123,'Mahdee')
+
+print(tuple) # print the entire tuple
+print(tuple[0]) # print the first element
+print(tuple[1:3]) # print the element 2nd to 3rd
+print(tuple[2:]) # print the element starting from 3rd element
+print(tuple*2) # print concatenated tuple
+print(tuple + tinyTuple)
