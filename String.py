53bf651b051c765cbef9b7dbdf30dd5804df3505
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-23 15:50:35 -0700

    4th chapter list index string

--- String.py
+++ String.py
@@ -10,3 +10,9 @@ print(program)
 
 # you cant use a double quote inside a double quote.
 # you cant use a single quote inside a single quote.
+
+new = "Jashore"
+format = new[0]
+formats = new[6]
+print(format)
+print(formats)
