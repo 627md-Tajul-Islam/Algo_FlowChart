10e6438428c35efdde29ac4218310d67c6db65ff
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-23 15:25:42 -0700

    4th chapter quotation

--- String.py
+++ String.py
@@ -2,3 +2,8 @@ str = 'Hello'
 str1 = "Hello"
 print(str)
 print(str1)
+
+program = "Python is a 'Programing Language'. I want to learn it"
+print(program)
+program = 'Python is a "Programing Language". I want to learn it'
+print(program)
