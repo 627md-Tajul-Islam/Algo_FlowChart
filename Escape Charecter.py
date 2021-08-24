4a4391361488ae781468ac2e0182ffb49312c12d
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-23 22:10:14 -0700

    4th chapter 1-7 esc chr added

--- Escape Charecter.py
+++ Escape Charecter.py
@@ -1,2 +1,17 @@
 # \e
 # this is used for escape
+
+# \'
+#for single quote
+
+# \"
+# for double quote
+
+# \n
+# to show your in a new line or give a line break
+
+# \a
+# to give an alert
+
+# /b
+# give a backslash
