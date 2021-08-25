a83cb462d0a719e03c45267b62664d3fb0cf8718
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-25 02:50:47 -0700

    4th chapter esc chr 4th added

--- Esc che exp2.py
+++ Esc che exp2.py
@@ -1,9 +1,14 @@
-"""
+
 print("Do you know \'Tajul\' is my name")
 print("Do you know \"Tajul\" is my name")
-"""
+
 
 print("Names: Tajul\\Pratasha\\Rakib\\sajjad\\keya\\")
 # put backslash in a line
 
 print('What\'s your name ?')
+
+
+print(r"sl\name\address\bdate")
+
+print(r"welcome\\")
