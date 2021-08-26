79529ed281ad5c9f040e5065b12a854cce7756cf
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-25 23:13:57 -0700

    4th chapter member added

--- Membership Operator.py
+++ Membership Operator.py
@@ -2,8 +2,17 @@
 # if  a character is in a specific string it will return true
 # if  a character is not in a specific string it will return false
 
+#1
 a = "j"
 b = "tajul"
 c = a in b
 print(c)
 # it will return true cause [j] is in the {tajul} word
+
+#2
+a = "T"
+b = "tajul"
+c = a in b
+print(c) # it will return false cause [T] is not in {tajul}
+
+#3
