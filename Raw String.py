990be6bbd7584eb319cc0cc38eff530bcb7eca71
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-25 22:03:07 -0700

    4th chapter esc chr double slash added

--- Raw String.py
+++ Raw String.py
@@ -1,3 +1,5 @@
 print("sl\name\address\birth")  # this output will not show perfectly
 
-print(r"sl\name\address\birth")
+print(r"sl\name\address\birth") # this will work
+
+print(r"welcome\\") # this will add double backslash
