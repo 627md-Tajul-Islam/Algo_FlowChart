c88e7e756627e8071038eeb66737b49f85535698
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-25 22:36:31 -0700

    4th chapter comments added

--- Concatenation operator.py
+++ Concatenation operator.py
@@ -14,3 +14,17 @@ b = "World"
 add = a + " " + "&" + " " + b
 print(add)  # adding space
 
+""" 
+a = "2"
+b = "2"
+add = a + b
+print(add) # it will return 22 instead of 4 cause its a string
+
+a = 1
+b = "2"
+c = 3
+d = "4"
+add = a + b + c + d
+print(add)
+"""
+# this will through an error cause its mixed with string with number
