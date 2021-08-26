43108d10f38daa099aef935a39309653ee4274c4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-25 22:29:05 -0700

    4th chapter comments added

--- Concatenation operator.py
+++ Concatenation operator.py
@@ -1,14 +1,16 @@
+#1
 a = "Hello"
 b = "World"
 add = a + b
 print(add)  # adding plain string
-
+#2
 a = "Hello"
 b = "World"
 add = a + "&" + b
 print(add)  # adding another text string
-
+#3
 a = "Hello"
 b = "World"
 add = a + " " + "&" + " " + b
 print(add)  # adding space
+
