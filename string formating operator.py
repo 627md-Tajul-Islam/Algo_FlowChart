72712a662939381bc2d83641be9c3dbce9193345
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-26 21:42:26 -0700

    4th chapter string formatter added

--- string formating operator.py
+++ string formating operator.py
@@ -1,11 +1,13 @@
+# 1
 b = " python"
 print('My favourite language is: %s' %b)
-
+#2
 x = "I have only %d %s" %(1000, 'Taka')
 print(x)
-
+#3
 x = 3.1416161616
 print(x)
 print('%.2f'%x)
 print('%.3f'%x)
 print('%.4f'%x)
+#4
