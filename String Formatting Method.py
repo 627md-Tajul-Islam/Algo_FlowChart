fb264047ab77ff5ccf6944da19d46e84f628a160
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-31 11:30:21 -0700

    4th chapter string formatting method added

--- String Formatting Method.py
+++ String Formatting Method.py
@@ -12,4 +12,7 @@ name = "Tajul"
 print('Hi, My name is {}|' .format(name)) #formatting variable using string format
 #4
 list = [1,2,3,4,5]
-print("The List is:{}".format(list))
+print("The List is:{}".format(list)) # formatting list
+#5
+msg = "My score on C: {0}, Python: {1},Java: {2}".format(6,6.5,5)
+print(msg) # using placeholders to using string
