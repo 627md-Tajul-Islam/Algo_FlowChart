93004800430d9441a24bf9105efd6824c2de50e0
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-31 10:52:00 -0700

    4th chapter string formatting method added

--- String Formatting Method.py
+++ String Formatting Method.py
@@ -1,5 +1,9 @@
 #1
 a = 10
 b = 20
-print('Value of a = {}, value of b = {}' .format(a,b))
+print('Value of a = {}, value of b = {}' .format(a,b)) # using multiple variable
 #2
+a = 10
+b = 50
+s = "Value of a = {}, value of b = {}"
+print(s.format(a,b)) #  format using a var, storing string into var
