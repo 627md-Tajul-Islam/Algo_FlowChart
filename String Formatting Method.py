710e815e1ae56f803340caa86d625c4df241fb09
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-31 11:02:09 -0700

    4th chapter string formatting method added

--- String Formatting Method.py
+++ String Formatting Method.py
@@ -6,4 +6,7 @@ print('Value of a = {}, value of b = {}' .format(a,b)) # using multiple variable
 a = 10
 b = 50
 s = "Value of a = {}, value of b = {}"
-print(s.format(a,b)) #  format using a var, storing string into var
+print(s.format(a,b)) #  format using a var, storing string into
+#3
+name = "Tajul"
+print('Hi, My name is {}|' .format(name)) #formatting variable
