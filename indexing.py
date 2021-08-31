e24ce5d622f05898556ef94fffea2f5a1568bd38
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-31 13:05:11 -0700

    4th chapter string indexing added

--- indexing.py
+++ indexing.py
@@ -1,3 +1,8 @@
 name = "Bangladesh"
 index = name[0]
 print(index)
+
+Name = "Bangladesh"
+index2 = Name[-1]
+print(index2)
+#negative number indexing starts from the last charectar and it starts from -1
