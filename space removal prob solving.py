22df4c555254a039dbfba06ea4fe0acc08015c3c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-04 21:26:51 -0700

    4th chapter string built in methods added

--- space removal prob solving.py
+++ space removal prob solving.py
@@ -1,3 +1,9 @@
 while true:
     print("Enter 'x' for exit.")
     string = input("Entrer Any String")
+    if string =='x':
+        break
+    else:
+        new_string = string.replace(" ","")
+        print("\nNew string after removing all spaces: ")
+        print(new_string, "\n")
