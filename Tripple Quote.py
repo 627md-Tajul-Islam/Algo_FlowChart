c3061a9f0df12a8c8ae01d534d0b45faf1641e0e
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-04 21:21:58 -0700

    4th chapter string built in methods added

--- Tripple Quote.py
+++ Tripple Quote.py
@@ -2,3 +2,5 @@ var = """
 I am tajul islam.I reads in JPI.My home town is jashore. I am a jr frontend developer who is working in a company named GGLink.My salary is 50000 BDT
 """
 print(var)
+
+print(u"Welcome to CPI") #unicode
