998633fe71b85a25e21167c44e89b1589b647357
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-04 21:46:27 -0700

    4th chapter string tripple queto added

--- String Comparison Prob solving.py
+++ String Comparison Prob solving.py
@@ -1,3 +1,12 @@
 while True:
     print("Enter 'x' for exit. ")
     string1 = input("Enter string 1: ")
+    string2 = input("Enter string 2: ")
+
+    if string1 == x:
+        break
+    else:
+        if string1 == string2:
+            print("They are all the same. /n")
+        else:
+            print
