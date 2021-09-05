3d2bb16dc5c91f3f753ce4e5a2676905698e956c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-04 23:14:14 -0700

    4th chapter string prob solving added

--- Input a string then make it reverse prob solving.py
+++ Input a string then make it reverse prob solving.py
@@ -2,6 +2,3 @@ while True:
     string = input("Enter any string: ")
     if string == 'x':
         break
-    else:
-        reverseString = string[::-1]
-        print("Reverse String =",reverseString,"\n")
