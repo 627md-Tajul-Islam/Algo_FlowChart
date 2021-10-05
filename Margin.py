413e9f62d37cd1ca027d05fa3aedd23c486e6255
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-04 20:10:42 -0700

    6th chapter string prob solving added

---
+++ Margin.py
@@ -0,0 +1,233 @@
+"""
+this line introduces sum of 2 numbers
+this line introduces sum of 2 numbersthis line introduces sum of 2 numbersthis line introduces sum of 2 numbers
+this line introduces sum of 2 numbersthis line introduces sum of 2 numbersthis line introduces sum of 2 numbersthis line introduces sum of 2 numbersthis line introduces sum of 2 numbersv
+"""
+#this is number one
+#this is number one
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
+if True:
+    print("Answer")
+    print("True")
+    print("End")
+else:
+    print("answer")
+
+print("False")
