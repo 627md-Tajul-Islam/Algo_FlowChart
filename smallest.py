ba91e2a19ecae9ed0ab0f00040fe3ba4845c1310
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-12-07 00:00:09 +0600

    7th chapter string prob solving added

--- smallest.py
+++ smallest.py
@@ -4,7 +4,7 @@ num3 = int(input("Enter third number: "))
 
 if num1 < num2 and num1 < num3:
     small = num1
-if num2 < num1 and num2 < num1:
+elif num2 < num1 and num2 < num1:
     small = num2
 else:
     small = num3
