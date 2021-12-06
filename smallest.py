f2f289efbcf0bc8a9c8d78090deb28dedc3b29ae
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-12-06 23:40:22 +0600

    7th chapter string prob solving added

--- smallest.py
+++ smallest.py
@@ -1,7 +1,6 @@
 num1 = int(input("Enter first number: "))
-num1 = int(input("Enter second number: "))
-num1 = int(input("Enter third number: "))
-
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
 
 if num1 < num2 and num1 < num3:
     small = num1
