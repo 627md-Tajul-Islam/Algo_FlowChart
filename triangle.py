9ba5e8d5af3d5f5b67a0c58487ce135a047ed713
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-12-07 00:14:51 +0600

    7th chapter string prob solving added

--- triangle.py
+++ triangle.py
@@ -4,3 +4,8 @@ b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
 if (a+b) > c and (b+c) >a and (c+a) > b:
+    s = (a+b+c)/2
+    Area = math.sqrt(S*(S-a)*(S-b)*(S-c))
+    print(Area)
+else:
+    print("Triangle not possible")
