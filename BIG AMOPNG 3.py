34edf7284fd7243b967076fe85040d6aa7584f0b
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-12-07 11:44:39 +0600

    7th chapter string prob solving added

--- BIG AMOPNG 3.py
+++ BIG AMOPNG 3.py
@@ -1,3 +1,10 @@
 a = int(input("Enter 1st number"))
 b = int(input("Enter 2nd number"))
 c = int(input("Enter 3rd number"))
+
+if a > b and a > c:
+    print("A is largest")
+elif b > a and b > c:
+    print("B is largest")
+else:
+    print("C is largest")
