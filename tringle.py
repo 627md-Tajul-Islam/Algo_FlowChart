fa404a5ccaac64e2eec7085221f1223b49d82037
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-12-07 11:51:54 +0600

    7th chapter string prob solving added

--- tringle.py
+++ tringle.py
@@ -5,3 +5,8 @@ b = int(input("Enter 2nd hand: "))
 c = int(input("Enter 3rd hand: "))
 
 if (a+b)>c and (b+c)>a and (c+a)>b:
+    S = a+b+c /2
+    Area = math.sqrt(S*(S-a)*(S-c)*(S-c))
+    print(Area)
+else:
+    print("Triangle not possible")
